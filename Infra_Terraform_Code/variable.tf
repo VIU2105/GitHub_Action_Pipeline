29e7b4a99c843b6c4ad91f2fb5f675afc3afffd3
unknown <vinod.beni@gmail.com>
2025-07-08 19:34:58 +0530

    Few Bug3 in variable type

--- Infra_Terraform_Code/variable.tf
+++ Infra_Terraform_Code/variable.tf
@@ -1,6 +1,10 @@
 variable "rg"{
-    type = "string"
+    type = string
 }
 variable "vnet"{
-    type = "string"
+    type = string
 }
+
+#Terraform 0.11 and earlier required type constraints to be given in quotes,
+#| but that form is now deprecated and will be removed in a future version of
+#│ Terraform. Remove the quotes around "string".
